16afbf617964bd8325d6ee5511eb0190c1372ca4
wiseandwealthycho <wiseandwealthycho@gmail.com>
2021-11-22 16:42:28 +0900

    hello world!

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res) {
+  res.send('Hello Beautiful World!');
+});
+
+var port = 3001;
+app.listen(port, function(){
+  console.log('Server On! http://localhost:'+port);
+});
